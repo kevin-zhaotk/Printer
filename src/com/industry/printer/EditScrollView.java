a011ff25754ac0e324f9c92b6607ab1ac6687b75
kevin zhao <kevin_zhaotk@163.com>
2016-04-11 21:29:18 +0800

    EditScrollview 只画可见部分，不可见部分不需要画

--- src/com/industry/printer/EditScrollView.java
+++ src/com/industry/printer/EditScrollView.java
@@ -64,34 +64,37 @@ public class EditScrollView extends View {
 		
 		for(BaseObject obj : mTask.getObjects())
 		{
+			/* 只有当cursor选中时才显示十字标线  */
 			if(obj instanceof MessageObject) {
+				if (!obj.getSelected()) {
+					continue;
+				}
+				
 				float[] points = {
 					/* 画水平线 */
 					0, obj.getY(), mScreenW, obj.getY(),
 					/* 画垂直线 */
 					obj.getX(), 0, obj.getX(), mScreenH
 				};
-				
-				if (!obj.getSelected()) {
-					p.setColor(Color.BLACK);
-				} else {
-					p.setColor(Color.RED);
-				}
+				p.setColor(Color.BLACK);
 				p.setStrokeWidth(2);
 				canvas.drawLines(points, p);
 				continue;
-			 }
-				 
-			 if(mContext == null)
-					Debug.d(TAG, "$$$$$$$$$$context=null");
-			 Bitmap bitmap = obj.getScaledBitmap(mContext);
-			 canvas.drawBitmap(bitmap, obj.getX(), obj.getY(), p);
-			 BinFromBitmap.recyleBitmap(bitmap);
+			}
+			/* 不在显示区域内的对象可以不画，优化效率  */
+			if ((obj.getXEnd() < getScrollX()) || (obj.getX() > getScrollX() + mParent.getWidth())) {
+				continue;
+			}
+			if(mContext == null)
+				Debug.d(TAG, "$$$$$$$$$$context=null");
+			Bitmap bitmap = obj.getScaledBitmap(mContext);
+			canvas.drawBitmap(bitmap, obj.getX(), obj.getY(), p);
+			BinFromBitmap.recyleBitmap(bitmap);
 			 
-		 }
-		 Debug.d(TAG, "<<<==onDraw");
+		}
+		Debug.d(TAG, "<<<==onDraw");
 		 //mParent.fling(100);
-	 } 
+	} 
 	
 	public void setTask(MessageTask task) {
 		mTask = task;
