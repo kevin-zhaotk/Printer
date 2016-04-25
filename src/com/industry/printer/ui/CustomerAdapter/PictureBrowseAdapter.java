73675da9747bc9d2399a560a210958e2c391d0b6
kevin zhao <kevin_zhaotk@163.com>
2016-04-25 19:39:37 +0800

    add picture dialog adapter“

---
+++ src/com/industry/printer/ui/CustomerAdapter/PictureBrowseAdapter.java
@@ -0,0 +1,120 @@
+package com.industry.printer.ui.CustomerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.industry.printer.R;
+import com.industry.printer.Utils.Debug;
+import com.industry.printer.ui.Items.PictureItem;
+
+
+import android.content.Context;
+import android.graphics.Color;
+import android.net.Uri;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+public class PictureBrowseAdapter extends BaseAdapter {
+
+	public static final String TAG="PreviewAdapter";
+	public int mChecked;
+	public int mlastChecked;
+	public Context mContext;
+	public boolean mManual;
+	
+	private LayoutInflater mInflater;
+	private List<PictureItem> mItems = new ArrayList<PictureItem>();
+	
+	private ViewHolder	mHolder;
+	
+	private class ViewHolder {
+		ImageView 	mImg;
+		TextView	mTitle;
+	}
+	
+	public PictureBrowseAdapter(Context context) {
+		mChecked=-1;
+		mlastChecked = -1;
+		mContext = context;
+		mManual = false;
+		mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	}
+
+	@Override
+	public View getView(final int position, View convertView, ViewGroup parent)
+	{
+		View view =null;
+		if(convertView != null)
+		{
+			mHolder = (ViewHolder) convertView.getTag();
+		}else
+		{
+			convertView = mInflater.inflate(R.layout.grid_item, null);
+			mHolder = new ViewHolder();
+			mHolder.mImg = convertView.findViewById(R.id.image);
+			mHolder.mTitle = convertView.findViewById(R.id.title);
+			convertView.setTag(mHolder);
+		}
+		if(mChecked == position)
+		{
+			
+		}
+		else
+		{
+			
+		}
+		mHolder.mImg.setImageURI(Uri.parse(mItems.get(position).getPath()));
+		mHolder.mTitle.setText(mItems.get(position).getTitle());
+		
+		return convertView;
+	}
+	
+	public void setChecked(int position)
+	{
+		mlastChecked = mChecked;
+		mChecked = position;
+	}
+	
+	public int getChecked()
+	{
+		return mChecked;
+	}
+	
+	public void setMode(boolean manual)
+	{
+		mManual = manual;
+	}
+	
+	public void setData(ArrayList<PictureItem> items) {
+		mItems.clear();
+		mItems.addAll(items);
+		notifyDataSetChanged();
+	}
+	
+	public void addItem(PictureItem item) {
+		mItems.add(item);
+	}
+
+	@Override
+	public int getCount() {
+		return mItems.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return null;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+}
---
+++ src/com/industry/printer/ui/CustomerDialog/PictureBrowseDialog.java
@@ -0,0 +1,97 @@
+package com.industry.printer.ui.CustomerDialog;
+
+import java.io.File;
+
+import com.industry.printer.R;
+import com.industry.printer.Utils.ConfigPath;
+import com.industry.printer.Utils.Debug;
+import com.industry.printer.Utils.PlatformInfo;
+import com.industry.printer.ui.CustomerAdapter.PictureBrowseAdapter;
+import com.industry.printer.ui.Items.PictureItem;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.EditText;
+import android.widget.GridLayout;
+import android.widget.GridView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+
+public class PictureBrowseDialog extends CustomerDialogBase implements android.view.View.OnClickListener, OnItemClickListener {
+
+	private static final String TAG = PictureBrowseDialog.class.getSimpleName();
+	
+	public RelativeLayout mConfirm;
+	public RelativeLayout mCancel;
+	public RelativeLayout mPagePrev;
+	public RelativeLayout mPageNext;
+	public EditText		  mSearch;
+	public GridView		  mPicView;	
+	public PictureBrowseAdapter mAdapter;
+	
+	public PictureBrowseDialog(Context context) {
+		super(context);
+	}
+
+	@Override
+	 protected void onCreate(Bundle savedInstanceState) {
+		Debug.d(TAG, "===>oncreate super");
+		 super.onCreate(savedInstanceState);
+		 Debug.d(TAG, "===>oncreate");
+		 this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		 this.setContentView(R.layout.picture_preview);
+		 
+		 mConfirm = (RelativeLayout) findViewById(R.id.btn_ok_message_list);
+		 mConfirm.setOnClickListener(this);
+		 
+		 mCancel = (RelativeLayout) findViewById(R.id.btn_cancel_message_list);
+		 mCancel.setOnClickListener(this);
+		 
+		 mPagePrev = (RelativeLayout) findViewById(R.id.btn_page_prev);
+		 mPagePrev.setOnClickListener(this);
+		 
+		 mPageNext = (RelativeLayout) findViewById(R.id.btn_page_next);
+		 mPageNext.setOnClickListener(this);
+		 
+		 mSearch = (EditText) findViewById(R.id.et_search);
+		 mSearch.setVisibility(View.GONE);
+		 mPicView = (GridView) findViewById(R.id.picture_grid);
+		 mPicView.setOnItemClickListener(this);
+		 
+		 mAdapter = new PictureBrowseAdapter(getContext());
+		 setupViews();
+		 load();
+		 mPicView.setAdapter(mAdapter);
+	 }
+	
+	private void setupViews() {
+		if (PlatformInfo.PRODUCT_FRIENDLY_4412.equals(PlatformInfo.getProduct())) {
+			mPagePrev.setVisibility(View.GONE);
+			mPageNext.setVisibility(View.GONE);
+		}
+	}
+
+	@Override
+	public void onClick(View v) {
+		
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+		
+	}
+	
+	public void load() {
+		File dir = new File(ConfigPath.getPictureDir());
+		File[] list = dir.listFiles();
+		for (File file : list) {
+			PictureItem item = new PictureItem(file.getAbsolutePath(), file.getName());
+			mAdapter.addItem(item);
+		}
+	}
+}
